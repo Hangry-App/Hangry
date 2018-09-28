83c400d6b8116feff17c5423219c90d3c826595b
Morgan Brown <Morganb816@gmail.com>
2018-09-28 14:30:35 -0400

    style: map can now be styled to our liking

--- components/Map.js
+++ components/Map.js
@@ -1,8 +1,145 @@
 import React, { Component } from "react";
 import { View, StyleSheet, Text, Platform } from "react-native";
-import { Constants, Location, Permissions, MapView } from "expo";
+import { Constants, Location, Permissions, MapView} from "expo";
 
 const Marker = MapView.Marker
+
+const mapStyle = [
+    {
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.neighborhood",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "landscape",
+      "stylers": [
+        {
+          "saturation": -100
+        },
+        {
+          "lightness": 100
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#8c8c8c"
+        },
+        {
+          "lightness": 70
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#5f5f5f"
+        },
+        {
+          "lightness": 75
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#5f5f5f"
+        },
+        {
+          "lightness": 100
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "stylers": [
+        {
+          "lightness": 100
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#d2d2d2"
+        },
+        {
+          "lightness": 80
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#10b1c1"
+        }
+      ]
+    }
+  ]
 
 export class Map extends Component {
   state = {
@@ -49,6 +186,8 @@ export class Map extends Component {
               latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
             }}
+            provider={MapView.PROVIDER_GOOGLE}
+            customMapStyle={mapStyle}
             showsUserLocation
             showsMyLocationButton
           />
