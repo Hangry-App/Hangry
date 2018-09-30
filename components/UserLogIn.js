2823dbcf5dd5e63ed3bc72856a85f4fab3270e35
John Riccardi <riccjohn@gmail.com>
2018-09-30 14:46:17 -0500

    Able to input email and password (which is hidden), updates state as you type, and submits an alert when you hit log in

--- components/UserLogIn.js
+++ components/UserLogIn.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import {
-  FormLabel,
-  FormInput,
-  FormValidationMessage,
-  Button,
-} from 'react-native-elements';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Alert, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { LinearGradient } from 'expo';
 
 class UserLogin extends Component {
   constructor() {
     super();
-    let state = {
+    this.state = {
       email: '',
       password: '',
     };
+    this._onPressButton = this._onPressButton.bind(this);
   }
+
+  _onPressButton() {
+    if (this.state.email && this.state.password) {
+      Alert.alert(
+        'Email: ' + this.state.email + ' ' + 'Password: ' + this.state.password
+      );
+    } else {
+      Alert.alert('Please enter email and password to log in');
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -23,16 +29,26 @@ class UserLogin extends Component {
           colors={['#59DFF8', '#0ED2F7']}
           style={styles.container}
         >
-          <FormLabel labelStyle={styles.text}>Email</FormLabel>
-          <FormInput
-            containerStyle={styles.form}
+          <Text style={styles.text}>Email</Text>
+          <TextInput
+            style={styles.form}
             defaultValue={'email@email.com'}
+            clearTextOnFocus={true}
+            autoCapitalize={'none'}
+            onChangeText={text => this.setState({ email: text })}
           />
-          <FormLabel labelStyle={styles.text}>Password</FormLabel>
-          <FormInput containerStyle={styles.form} defaultValue={'password'} />
+          <Text style={styles.text}>Password</Text>
+          <TextInput
+            style={styles.form}
+            defaultValue={'password'}
+            clearTextOnFocus={true}
+            secureTextEntry={true}
+            autoCapitalize={'none'}
+            onChangeText={text => this.setState({ password: text })}
+          />
           <Button
-            buttonStyle={styles.button}
-            textStyle={styles.buttonText}
+            onPress={this._onPressButton}
+            style={styles.button}
             title="Log In"
           />
         </LinearGradient>
@@ -59,6 +75,7 @@ const styles = StyleSheet.create({
   },
   button: {
     width: '60%',
+    height: 20,
     backgroundColor: '#fff',
   },
   buttonText: {
