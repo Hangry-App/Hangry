b25e45145ab320f171af73e580c40c66ce422203
John Riccardi <riccjohn@gmail.com>
2018-10-04 10:42:35 -0500

    Feature: Added price and restaurant rating to card that changes with each restaurant, and got swipe up / down working

--- components/Cards.js
+++ components/Cards.js
@@ -34,6 +34,22 @@ class Cards extends Component {
     };
   }
 
+  generateRating = item => {
+    let rating = '';
+    for (let i = 0; i < Math.floor(item.rating); i++) {
+      rating += '*';
+    }
+    return <Text>{rating}</Text>;
+  };
+
+  generatePrice = item => {
+    let price = '';
+    for (let i = 0; i < Math.floor(item.price.tier); i++) {
+      price += '$';
+    }
+    return <Text>{price}</Text>;
+  };
+
   onSwipeUp(gestureState) {
     this.setState({ cardHeight: 400 });
   }
@@ -48,70 +64,83 @@ class Cards extends Component {
 
   _keyExtractor = item => item.restaurantId;
 
-  _renderItem = ({ item }) => (
-    <View style={styles.padCard}>
-      <GestureRecognizer
-        onSwipeUp={state => this.onSwipeUp(state)}
-        onSwipeDown={state => this.onSwipeDown(state)}
-        config={{
-          velocityThreshold: 0.3,
-          directionalOffsetThreshold: 80,
-        }}
-      >
-        <View style={styles.restaurantCard}>
-          {/*Top of card - rest name, ratings, etc.*/}
-          <View style={styles.cardHeader}>
-            <Text style={styles.boldWhite}>{item.name}</Text>
-            <View style={styles.row}>
-              <Text
-                style={{
-                  color: 'white',
-                  fontWeight: 'bold',
-                  textAlign: 'center',
-                }}
-              >
-                *****
-              </Text>
-              <Text
-                style={{
-                  color: 'white',
-                  fontWeight: 'bold',
-                  textAlign: 'center',
-                }}
-              >
-                $$$
-              </Text>
+  _renderItem = ({ item }) => {
+    return (
+      <View style={styles.padCard}>
+        <GestureRecognizer
+          onSwipeUp={state => this.onSwipeUp(state)}
+          onSwipeDown={state => this.onSwipeDown(state)}
+          config={{
+            velocityThreshold: 0.3,
+            directionalOffsetThreshold: 80,
+          }}
+        >
+          <View
+            style={
+              (styles.restaurantCard,
+              {
+                height: this.state.cardHeight,
+                backgroundColor: '#fff',
+                shadowColor: '#000',
+                borderRadius: 4,
+                shadowOffset: { width: 0, height: 2 },
+                shadowRadius: 4,
+                shadowOpacity: 0.4,
+              })
+            }
+          >
+            {/*Top of card - rest name, ratings, etc.*/}
+            <View style={styles.cardHeader}>
+              <Text style={styles.boldWhite}>{item.name}</Text>
+              <View style={styles.row}>
+                <Text
+                  style={{
+                    color: 'white',
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                  }}
+                >
+                  {this.generateRating(item)}
+                </Text>
+                <Text
+                  style={{
+                    color: 'white',
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                  }}
+                >
+                  {this.generatePrice(item)}
+                </Text>
+              </View>
+            </View>
+            <View style={styles.cardBody}>
+              {/* Check if there's a menu key on the restaurant object, and make sure it's an array*/}
+              {item.menu && Array.isArray(item.menu) && item.menu[0] ? (
+                <View style={styles.foodCard}>
+                  <Text style={styles.foodTitle}>{item.menu[0].name}</Text>
+                  {item.menu[0].description ? (
+                    <Text>{item.menu[0].description}</Text>
+                  ) : (
+                    <Text />
+                  )}
+
+                  {item.menu[0].description && item.menu[0].price ? (
+                    <Text style={styles.price}>${item.menu[0].price}</Text>
+                  ) : (
+                    <Text />
+                  )}
+                </View>
+              ) : (
+                <Text>Menu not available</Text>
+              )}
             </View>
           </View>
-          <View style={styles.cardBody}>
-            {/* Check if there's a menu key on the restaurant object, and make sure it's an array*/}
-            {item.menu && Array.isArray(item.menu) && item.menu[0] ? (
-              <View style={styles.foodCard}>
-                <Text style={styles.foodTitle}>{item.menu[0].name}</Text>
-                {item.menu[0].description ? (
-                  <Text>{item.menu[0].description}</Text>
-                ) : (
-                  <Text />
-                )}
-
-                {item.menu[0].description && item.menu[0].price ? (
-                  <Text style={styles.price}>${item.menu[0].price}</Text>
-                ) : (
-                  <Text />
-                )}
-              </View>
-            ) : (
-              <Text>Menu not available</Text>
-            )}
-          </View>
-        </View>
-      </GestureRecognizer>
-    </View>
-  );
+        </GestureRecognizer>
+      </View>
+    );
+  };
 
   render() {
-    console.log(this.props.restaurants[1].menu[0].name);
-    // console.log('STATE: ', this.state);
     return (
       <View style={styles.horizontalCardStrip}>
         <FlatList
@@ -121,6 +150,7 @@ class Cards extends Component {
           snapToAlignment="center"
           alwaysBounceHorizontal
           onViewableItemsChanged={this.onViewableItemsChanged}
+          extraData={this.state}
           data={this.props.restaurants}
           keyExtractor={this._keyExtractor}
           renderItem={this._renderItem}
